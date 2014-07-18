78f9962339625655dcd5b3854fe35db56023ca68
Jon Stockdill <jons@ziji-media.com>
2014-07-17 21:23:22 -0400

    Adding groovy hoe and lava lamp changes

--- src/main/java/com/minecraft/groovymod/GroovyMod.java
+++ src/main/java/com/minecraft/groovymod/GroovyMod.java
@@ -6,22 +6,31 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockTorch;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ChestGenHooks;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.util.EnumHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 @Mod(modid = GroovyMod.MODID, name = GroovyMod.NAME, version = GroovyMod.VERSION)
 public class GroovyMod {
@@ -50,17 +59,88 @@ public class GroovyMod {
 		}
 	}
 	
+	public class GroovyScoopySnack extends ItemFood{
+
+		public GroovyScoopySnack(int arg0, float arg1, boolean arg2) {
+			super(5, 0.9F, false);
+		}
+		
+		
+		@Override
+		public ItemStack onEaten(ItemStack arg0, World arg1, EntityPlayer player) {
+			
+			player.jump();  // can we put this in a thread and make the player jump repeatedly afterwards?
+			return super.onEaten(arg0, arg1, player);
+		}
+	}
+	
 	public class GroovyPickaxe extends ItemPickaxe{
 
 		protected GroovyPickaxe() {
 			super(groovyToolMaterial);
 		}
+		
+		 @Override
+		public boolean onBlockDestroyed(ItemStack itemStack,
+				World world, Block block, int p_150894_4_,
+				int p_150894_5_, int p_150894_6_, EntityLivingBase entityLivingBase) {
+			 
+			 
+			 //5% of the time drop a diamond if the block was coal
+			 if(!world.isRemote && 1 == new Random().nextInt(20) && "tile.oreCoal".equals(block.getUnlocalizedName())){
+				 EntityItem dropItem = entityLivingBase.entityDropItem(new ItemStack(Items.diamond), 1.0F);
+			 }
+
+			return super.onBlockDestroyed(itemStack, world, block,
+					p_150894_4_, p_150894_5_, p_150894_6_, entityLivingBase);
+		}
+
+	}
+
+	public class GroovyHoe extends ItemHoe{
+
+		protected GroovyHoe() {
+			super(groovyToolMaterial);
+
+			setUnlocalizedName("GroovyHoe");
+			setCreativeTab(CreativeTabs.tabTools);
+			setTextureName(GroovyMod.MODID + ":groovy_hoe");
+		}
 
 	}
 	
-	
 	public class GroovyLavalamp extends BlockTorch {
 
+		@Override
+		@SideOnly(Side.CLIENT)
+		public void randomDisplayTick(World world, int x, int y, int z, Random rand) {
+			
+			int l = world.getBlockMetadata(x, y, z);
+	        double d0 = (double)((float)x + 0.5F);
+	        double d1 = (double)((float)y + 0.7F);
+	        double d2 = (double)((float)z + 0.5F);
+	        double d3 = 0.2199999988079071D;
+	        double d4 = 0.27000001072883606D;
+
+        	world.spawnParticle("slime", d0 - d4, d1 + d3, d2, 0.0D, 0.0D, 0.0D);
+        	
+		}
+		
+		public void setFire(Entity entity){
+			entity.setFire(new Random().nextInt(10));
+		}
+		
+		@Override
+		public void onEntityWalking(World world, int x, int y, int z, Entity entity) {
+			setFire(entity);
+		}
+		
+		@Override
+		public void onFallenUpon(World world, int x, int y, int z,
+				Entity entity, float p_149746_6_) {
+			setFire(entity);
+			
+		}
 	}
 	
 	
@@ -97,6 +177,7 @@ public class GroovyMod {
 	public static Item groovy_boots;
 	public static Item groovy_sword;
 	public static Item groovy_pickaxe;
+	public static Item groovy_hoe;
 	public static Block groovy_lavalamp;
 	
 	//Create the material to make the groovy armor
@@ -111,6 +192,17 @@ public class GroovyMod {
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 
+		instantiateItems();
+		
+		registerItems();
+		
+		initializeRecipes();
+		
+		
+	}
+
+
+	private void instantiateItems() {
 		groovy_helmet = new GroovyArmor(groovyArmorMaterial, 0, GroovyArmorType.helmet.getTypeNum())
 		 .setUnlocalizedName("GroovyHelmet")
 		 .setCreativeTab(CreativeTabs.tabCombat)
@@ -141,6 +233,8 @@ public class GroovyMod {
 		.setCreativeTab(CreativeTabs.tabTools)
 		.setTextureName(GroovyMod.MODID + ":groovy_pickaxe");
 		
+		groovy_hoe = new GroovyHoe();
+		
 		groovy_lavalamp = new GroovyLavalamp()
 		.setBlockTextureName(GroovyMod.MODID + ":groovy_lavalamp")
 		.setHardness(0.0F)
@@ -148,19 +242,19 @@ public class GroovyMod {
 		.setLightLevel(0.9999F)
 		.setStepSound(Block.soundTypeAnvil)
 		.setBlockName("GroovyLavalamp");
-		
-		
+	}
+
+
+	private void registerItems() {
 		GameRegistry.registerItem(groovy_helmet, "groovy_helment");
 		GameRegistry.registerItem(groovy_chestplate, "groovy_chestplate");
 		GameRegistry.registerItem(groovy_leggings, "groovy_leggings");
 		GameRegistry.registerItem(groovy_boots, "groovy_boots");
 		GameRegistry.registerItem(groovy_sword, "groovy_sword");
 		GameRegistry.registerItem(groovy_pickaxe, "groovy_pickaxe");
+		GameRegistry.registerItem(groovy_hoe, "groovy_hoe");
 		
 		GameRegistry.registerBlock(groovy_lavalamp, "Groovy Lavalamp");
-		
-		initializeRecipes();
-		
 	}
 
 
@@ -214,6 +308,16 @@ public class GroovyMod {
 		//Pickaxe recipe
 		GameRegistry.addRecipe(new ItemStack(groovy_pickaxe), new Object[]{
 	    	"YDY", 
+	    	" S ",
+	    	" S ",
+	    	'Y', Blocks.yellow_flower,
+	    	'D', Items.diamond,
+	    	'S', Items.stick
+		});
+		
+		//Hoe recipe
+		GameRegistry.addRecipe(new ItemStack(groovy_hoe), new Object[]{
+	    	"YD ", 
 	    	" S ",
 	    	" S ",
 	    	'Y', Blocks.yellow_flower,
